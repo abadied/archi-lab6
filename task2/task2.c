74513a64dccace245aa4f0203035fa4b3e283f9e
sfinder <shahaffind@gmail.com>
2017-05-20 00:04:17 +0300

    :smile:

--- task2/task2.c
+++ task2/task2.c
@@ -15,57 +15,60 @@ int main(void)
     int     fd[2];
     pid_t   childpid1;
     pid_t   childpid2;
-    int* status = 0;
+    int status = 0;
     pipe(fd);
-    char* const args1[2] = {"ls","-l"}; 
-    
-    char* const args2[3] = {"tail","-n","2"}; 
-    
+    char* const args1[3] = {"/bin/ls", "-l", 0}; 
+
+    char* const args2[4] = {"tail","-n","2", 0}; 
+
     if((childpid1 = fork()) == -1)
     {
         perror("fork");
         exit(1);
     }
 
-    if(childpid1 == 0)
+    else if(childpid1 == 0)
     {
-        close(fd[1]);
+        close(1);
         dup(fd[1]);
         close(fd[1]);
-        
+
         execvp(args1[0],args1);
+        exit(0);
     }
     else
     {
-            /* Parent process closes up output side of pipe */
-            close(fd[1]);
+        /* Parent process closes up output side of pipe */
+        close(fd[1]);
 
-    }
-   
-    if((childpid2 = fork()) == -1)
-    {
+        if((childpid2 = fork()) == -1)
+	    {
             perror("fork");
             exit(1);
-    }
+	    }
 
-    if(childpid2 == 0)
-    {
-     
-            close(fd[0]);
+	    if(childpid2 == 0)
+	    {
+            close(0);
             dup(fd[0]);
             close(fd[0]);
-        
+	    	/*char readbuffer[80];
+			read(fd[0], readbuffer, 80);
+            printf("Received string: %s", readbuffer);
+			*/
             execvp(args2[0],args2);
-    }
-    else
-    {
-      
+            exit(0);
+	    }
+	    else
+	    {
             close(fd[0]);
+
+            waitpid(childpid1,&status,0);
+		    status = 0;
+		    waitpid(childpid2,&status,0);
+	    }
+
     }
-    
-    waitpid(childpid1,status,0);
-    status = 0;
-    waitpid(childpid2,status,0);
     
     return(0);
 }
